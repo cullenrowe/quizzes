b41e92a1e8acfedf3a186903114445eb0a0b9c12
cullenrowe <87037894+cullenrowe@users.noreply.github.com>
2026-09-10 15:14:32 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Paste your Apps Script web app URL between the quotes. It ends in /exec.
+// You only do this once. Every quiz uses the same address.
+
+window.QUIZ_SERVER = "PASTE_YOUR_WEB_APP_URL_HERE";
