c90d9b4d8e8ded7375a6c92b2146f56282aceebe
cullenrowe <87037894+cullenrowe@users.noreply.github.com>
2026-09-10 15:14:55 -0500

    Update QUIZ_SERVER URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your Apps Script web app URL between the quotes. It ends in /exec.
 // You only do this once. Every quiz uses the same address.
 
-window.QUIZ_SERVER = "PASTE_YOUR_WEB_APP_URL_HERE";
+window.QUIZ_SERVER = "https://script.google.com/macros/s/AKfycbwjKeLzDEaw9ddAN8HgzzO0DrQUBZPRx33ASZbGyYLwPTr11Kiu5QhCF6hVTMhXJz3q/exec";
